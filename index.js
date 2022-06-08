d13f30a2841213c6d6ce8a37b7107119ebb18c39
Transirizo <transirizo@icloud.com>
2022-06-08 17:27:39 +0800

    ui

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const Note = require("./model/note");
+const Note = require("app/models/note");
 
 let notes = [
 	{
