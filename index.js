525fa602e2b6fdd92921b403cbeb522fdae6c580
Transirizo <transirizo@icloud.com>
2022-06-09 16:04:25 +0800

    ui

--- index.js
+++ index.js
@@ -68,16 +68,46 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.get("/api/notes/:id", (req, res) => {
-	Note.findById(req.params.id).then((note) => {
-		res.json(note);
-	});
+	Note.findById(req.params.id)
+		.then((note) => {
+			if (note) {
+				res.json(note);
+			} else {
+				res.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
 });
 
-app.delete("/api/notes/:id", (req, res) => {
-	const id = Number(req.params.id);
-	notes = notes.filter((note) => note.id !== id);
-	res.status(204).end();
+app.delete("/api/notes/:id", (req, res, next) => {
+	Note.findByIdAndRemove(req.params.id)
+		.then(res.status(204).end())
+		.catch((error) => next(error));
 });
+
+app.put("/api/notes/:id", (req, res, next) => {
+	const body = req.body;
+	const note = {
+		content: body.content,
+		important: body.important,
+	};
+	Note.findByIdAndUpdate(req.params.id, note, { new: true })
+		.then((updatedNote) => {
+			res.json(updatedNote);
+		})
+		.catch((error) => next(error));
+});
+
+const errorHandler = (error, req, res, next) => {
+	console.error(error.message);
+
+	if (error.name === "CastError") {
+		return res.status(400).send({ error: "malformatted id" });
+	}
+	next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
